f574315ad1448d2499ec0dc41ac3d34746bd11bb
Mohit Mohlia <mohitmohlia96@gmail.com>
2025-05-29 16:21:43 +0530

    data

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import Big from "big.js";
+import transactions from "../transactions.json";
+
+interface Transaction {
+  date_transacted: string;
+  date_posted: string;
+  amount: number;
+  description: string;
+  currency: string;
+}
+
+interface Transactions {
+  transactions: Transaction[];
+}
+
+function calculate(transactions: Transaction[]) {
+  let total = new Big(0);
+
+  for (const transaction of transactions) {
+    total = total.plus(transaction.amount);
+  }
+  console.log("Total:", total.toString());
+}
+
+calculate(transactions.transactions);
